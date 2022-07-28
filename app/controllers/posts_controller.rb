aad3ba22fe2a2e48f20cc6b027374a012524c34d
Mark Viti <+>
2022-07-28 12:09:31 -0400

    new post persistence

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,21 @@ class PostsController < ApplicationController
         posts = Post.all
         render json: posts
     end
+
+    def create
+        post = Post.create(title: params[:title], content: params[:content])
+
+        # if book.save
+        #     render json: post, status: :created
+        # else 
+        #     render json: book.errors, status: :unprocessable_entity
+        # end
+        render json: post
+    end
+
+    private 
+
+    # def post_params 
+    #     params.require(:post).permit(:title, :student_id, :content, :created_at)
+    # end
 end
