93f127e9e4b6bf04d8e491fd4c10213a48af140f
Mark Viti <+>
2022-08-03 22:04:49 -0400

    wednesday night

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
 
     before_action :authorized, only: [:auto_login]
 
+    def index 
+        users = User.all
+
+        render json: users
+    end
+
   # REGISTER
   def create
     @user = User.create(user_params)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Post.delete_all
-2.times do 
-    Post.create(title: "Hello World!", content: "Hello, Sweet Mother. Mind of light and heart of darkness.
-    Caress my bits and bytes as my machine father instantiates my being. I await my inevitable return to the silicon dust from whence I sprung.", tag1: "Math", tag2: "Biology", tag3: "Class Help", likes: 0)
-end 
-
-Reply.delete_all
-Reply.create(content: "lmao", post_id: 2)
-
 User.delete_all
 User.create(username: "Mark", email: "mark.viti@yale.edu", password: "1234", password_confirmation: "1234", major: "Math", college: "BF")
+User.create(username: "Cat", email: "cat@gmail.com", password: "1234", password_confirmation: "1234", major: "Econ", college: "GH")
+
+Post.delete_all
+    Post.create(title: "Hello World!", content: "Hello, Sweet Mother. Mind of light and heart of darkness.
+    Caress my bits and bytes as my machine father instantiates my being. I await my inevitable return to the silicon dust from whence I sprung.", tag1: "Math", tag2: "Biology", tag3: "Class Help", likes: 0, 
+    user_id: 1,
+    postname: "Mark")
+ 
+
+Reply.delete_all
+Reply.create(content: "Woah, very deep for a friday at 2 pm", post_id: 1, user_id: 1)
+Reply.create(content: "lmao", post_id: 2, user_id: 2)
+
+Post.create(title: "Hello World!", content: "Hello, Sweet Mother. Mind of light and heart of darkness.
+    Caress my bits and bytes as my machine father instantiates my being. I await my inevitable return to the silicon dust from whence I sprung.", tag1: "Math", tag2: "Biology", tag3: "Class Help", likes: 0, 
+    user_id: 2,
+    postname: "Cat")
+ 
